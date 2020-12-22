fc3fcee45f554a2991b2f5b1c9c1c0564e33158c
Martin Vysny <mavi@vaadin.com>
2020-12-22 16:28:20 +0200

    minor

--- src/main/java/org/acme/servlet/MyServlet.java
+++ src/main/java/org/acme/servlet/MyServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
  * @author Martin Vysny <mavi@vaadin.com>
  */
 @WebServlet(urlPatterns = { "/*" }, initParams = {
+        // workaround for https://github.com/mvysny/vaadin-quarkus/issues/2
         @WebInitParam(name = FrontendUtils.PROJECT_BASEDIR, value = "/home/mavi/work/inacta/vaadin-quarkus")
 }, asyncSupported = true)
 public class MyServlet extends VaadinServlet {
