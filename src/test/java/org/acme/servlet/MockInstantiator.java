993381aa5c47da11532afd4d703fef50c9ac9d00
Martin Vysny <mavi@vaadin.com>
2021-01-19 16:47:08 +0200

    Removed unnecessary @Unremovable from MockInstantiator

--- src/test/java/org/acme/servlet/MockInstantiator.java
+++ src/test/java/org/acme/servlet/MockInstantiator.java
@@ -5,7 +5,6 @@ import com.urosporo.quarkus.vaadin.cdi.QuarkusInstantiator;
 import com.urosporo.quarkus.vaadin.cdi.annotation.VaadinServiceEnabled;
 import com.urosporo.quarkus.vaadin.cdi.annotation.VaadinServiceScoped;
 import com.vaadin.flow.component.polymertemplate.TemplateParser;
-import io.quarkus.arc.Unremovable;
 import io.quarkus.test.Mock;
 
 /**
@@ -13,7 +12,6 @@ import io.quarkus.test.Mock;
  */
 @VaadinServiceScoped
 @VaadinServiceEnabled
-@Unremovable
 @Mock
 public class MockInstantiator extends QuarkusInstantiator {
     @Override
